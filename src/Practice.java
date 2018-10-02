bf5bc5c39ad694206f06558e82ebcb6b6183b9d3
Student675 <moses.tibambuya.2022@student.lhschools.org>
2018-10-02 07:24:26 -0400

    changes were made over the weekend

---
+++ src/Practice.java
@@ -0,0 +1,19 @@
+public class Practice {
+    public static void main(String[]args){
+
+boolean value = true;
+
+        int number=0;
+while(value){
+
+    System.out.println("I am looping this code "+number+" times");
+    if(number>10){
+        value=false;
+    }
+    else{
+        number++;
+    }
+
+
+    }
+}
